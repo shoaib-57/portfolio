68464676d33bc0158bccdf2c87636cd9590108fa
shoaib-57 <workshoaib6797@gmail.com>
2025-12-19 22:24:57 +0530

    initial project upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// ===== ACTIVE NAV LINK ON SCROLL =====
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (scrollY >= sectionTop - 150) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
